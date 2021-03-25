fc8192a3fc20b6a85ea1819cb564442ef2d23915
NelsonBeard <ilnur111111yandex.ru>
2021-03-25 23:56:00 +0500

    Выполнил задание 1

--- src/com/company/Lesson_2/Main.java
+++ src/com/company/Lesson_2/Main.java
@@ -1,4 +1,21 @@
 package com.company.Lesson_2;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void invertArray () {
+        int[] array = {1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1};
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == 1){
+                array[i] = 0;
+            }else{
+                array[i] = 1;
+            }
+        }
+        System.out.println (Arrays.toString(array));
+    }
+    public static void main(String[] args){
+    invertArray();
+    }
 }
