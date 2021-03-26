f2baf5c5f8d3e623ecef986893cf358c5db5ad5c
NelsonBeard <ilnur111111yandex.ru>
2021-03-26 10:35:01 +0500

    Выолнил задание 2

--- src/com/company/Lesson_2/Main.java
+++ src/com/company/Lesson_2/Main.java
@@ -4,18 +4,29 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void invertArray () {
+    public static void invertArray() {
         int[] array = {1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1};
-        for (int i = 0; i < array.length; i++){
-            if (array[i] == 1){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
                 array[i] = 0;
-            }else{
+            } else {
                 array[i] = 1;
             }
         }
         System.out.println (Arrays.toString(array));
     }
+
+    public static void fillArray() {
+        int[] array = new int[8];
+        int incVal = 3;
+        for (int i = 1; i < array.length; i++) {
+            array[i] = array[i-1] + incVal;
+        }
+        System.out.println (Arrays.toString(array));
+    }
+
     public static void main(String[] args){
     invertArray();
+    fillArray();
     }
 }
