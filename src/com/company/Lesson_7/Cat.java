b4ee224cccd2d5501ab00a394606a6bf2f136ff6
NelsonBeard <ilnur111111yandex.ru>
2021-04-12 14:09:34 +0500

    Внес изменения: о невозможности поесть решает класс Cat, а не Plate; добавил поле СЫТОСТЬ в класс Cat; добавил вывод информации он том, поел кот или нет.

--- src/com/company/Lesson_7/Cat.java
+++ src/com/company/Lesson_7/Cat.java
@@ -3,11 +3,21 @@ package com.company.Lesson_7;
 public class Cat {
     private String name;
     private int appetite;
+    private boolean satiety;
+
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
     }
+
     public void eat(Plate p) {
-        p.decreaseFood(appetite);
+        int food = p.getFood();
+        if (food > appetite) {
+            p.decreaseFood(appetite);
+            satiety = true;
+            System.out.println("Кот " + name + " поел.");
+        } else {
+            System.out.println("Коту " + name + " не хватает еды. Кот не стал есть.");
+        }
     }
 }
--- src/com/company/Lesson_7/Main.java
+++ src/com/company/Lesson_7/Main.java
@@ -2,10 +2,14 @@ package com.company.Lesson_7;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Barsik", 102);
+        Cat cat1 = new Cat("Barsik", 10);
+        Cat cat2 = new Cat("Bars", 100);
+        Cat cat3 = new Cat("Bar", 10);
         Plate plate = new Plate(100);
         plate.info();
         cat1.eat(plate);
+        cat2.eat(plate);
+        cat3.eat(plate);
         plate.info();
 
     }
